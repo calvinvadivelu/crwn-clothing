e7dee359079da174b4835ded865c16c6b3d49c6d
calvinvadivelu <cvadivelu@gmail.com>
2019-11-05 22:33:13 -0500

    setting up routing for main homepage menu items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+      </Switch>
+      
     </div>
   );
 }
